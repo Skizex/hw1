5750b64aa69a949a66f4d2cd6b76011b0a6364ef
Skizex <justskizex@gmail.com>
2023-10-30 17:14:19 +0400

    Второе задание

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введитее число:");
+string? input = Console.ReadLine();
+int number  = Convert.ToInt32(input);
+
+if(number>100)
+{
+    Console.WriteLine(input[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
