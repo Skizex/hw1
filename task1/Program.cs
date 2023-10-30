2896ccbf6ba646c9bc74e277c6f97b7fb8083c14
Skizex <justskizex@gmail.com>
2023-10-30 16:59:36 +0400

    Первое задание

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введитее трехзначное число:");
+string? input = Console.ReadLine();
+int number  = Convert.ToInt32(input);
+
+if(number>99 && number<999)
+{
+    Console.WriteLine($"{input[1]}");
+}
+else
+{
+    Console.WriteLine("Необходимо ввести   трехзначное число!");
+    
+}
+
+
+
