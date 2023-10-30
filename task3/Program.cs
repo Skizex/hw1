89eaf2a789b9681ec5deadc8b734df95e535cf58
Skizex <justskizex@gmail.com>
2023-10-30 17:27:37 +0400

    Hw

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Введите порядковый номер дня недели, чтобы проверить  является ли он выходным");
+int number =  Convert.ToInt32(Console.ReadLine());
+
+if(number ==  6  || number  == 7)
+{
+    Console.WriteLine("Это  выходной");
+
+}
+else if (number > 7)
+{
+    Console.WriteLine("В неделе только семь дней");
+}
+else
+{
+    Console.WriteLine("Это рабочий день");
+}
+
+
